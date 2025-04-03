153eb446a8e9e7d145d92e748cd017fabcea57ce
sriram507 <isreeram2014@gmail.com>
2025-04-03 20:29:17 +0530

    Added new code for props examples

--- src/home/homeComp.jsx
+++ src/home/homeComp.jsx
@@ -2,7 +2,7 @@ import { Component } from "react";
 import Footer from "../footer/footerComp";
 import HeaderComp from "../header/headerComp";
 import Main from "../main/mainComp";
-
+import Menu from "../menu/menu";
 class Home extends Component {
   render() {
     return (
@@ -14,12 +14,15 @@ class Home extends Component {
          //height: '100vh' // Set the height to 100% of the viewport height
           }}
       >
-        <HeaderComp />
-        <Main />
-        <Footer />
+        {/* <HeaderComp /> */}
+        {/* <Main /> */}
+        {/* <Footer /> */}
+        <Menu name="john" city="hyd" gender="male" />
       </div>
     );
   }
 }
 
 export default Home;
+
+
---
+++ src/menu/menu.js
@@ -0,0 +1,9 @@
+function Menu(props){
+    return(<>
+    <h1>{props.name}</h1>
+    <h1>{props.city}</h1>
+    <h1>{props.gender}</h1>
+    </>)
+}
+export default Menu;
+
